da6e530b9215f960f8bd0e4f3fb0b6190304bb0e
Ashani Ray <ashaniray@gmail.com>
2015-07-23 18:28:32 +0530

    added recaptcha

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,17 @@
+require 'uri'
+require 'net/http'
+
 class WelcomeController < ApplicationController
 
   def index
   end
 
   def submit
-    puts params
-    redirect_to welcome_index_path
+    if verify_recaptcha
+      render welcome_index_path
+    else
+      render welcome_getverified_path
+    end
   end
 
 end
---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,9 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LdTJwoTAAAAACB_61K0R4mnuyx9liBaj3ryVJRW'
+  config.private_key = '6LdTJwoTAAAAAAa6ol3R_FoefLPbAHEqhovCa9ka'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+  # Uncomment if you want to use the newer version of the API,
+  # only works for versions >= 0.3.7:
+  # config.api_version = 'v2'
+end
